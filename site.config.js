04f47635acb7422f7e0352b00f3678c0fada047b
raining001 <54823322+raining001@users.noreply.github.com>
2021-06-01 15:16:53 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'https://www.notion.so/blog-7a8c6c7740c842eb81a24324c101201f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'L',
+  domain: 'none',
+  author: 'lraining',
 
   // open graph metadata (optional)
   description: 'Example site description',
